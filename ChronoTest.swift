e70a4a313dd71e461023900e815fbb8106790e3a
thebnbrkr <51245304+thebnbrkr@users.noreply.github.com>
2023-06-14 16:35:16 -0700

    Update ChronoTest.swift

--- ChronoTest.swift
+++ ChronoTest.swift
@@ -1,11 +1,10 @@
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
             Button("Plain") {
-                testChrono(query: "Show me the data that was recorded on Boxing Day")
+                testChrono(query: "Show me the data that was recorded on Thanksgiving 2019")
             }
             .buttonStyle(.plain)
             .frame(maxWidth: .infinity)
@@ -18,134 +17,71 @@ struct ContentView: View {
 func testChrono(query: String) {
     let chrono = Chrono()
     
-    // Replace "Christmas" with "25th of December"
-    let modifiedQuery = query.replacingOccurrences(of: "Christmas", with: "25th of December", options: .caseInsensitive)
+    let replacements: [(pattern: String, replacement: String)] = [
+        ("Christmas", "25th of December"),
+        ("new year", "1st of January"),
+        ("Islander Day", "third Monday of February"),
+        ("Family Day", "third Monday of February"),
+        ("Louis Riel Day", "third Monday of February"),
+        ("Heritage Day", "third Monday of February"),
+        ("Valentine's Day", "14th of February"),
+        ("St\\. Patrick's Day", "17th of March"),
+        ("Good Friday", "7th of April"),
+        ("Easter Monday", "10th of April"),
+        ("Mother's Day", "second Sunday of May"),
+        ("Victoria Day", "third Monday of May"),
+        ("Father's Day", "third Sunday of June"),
+        ("Aboriginal Day", "21st of June"),
+        ("St\\. Jean Baptiste Day", "24th of June"),
+        ("Canada Day", "1st of July"),
+        ("Civic Holiday", "first Monday of August"),
+        ("Labour Day", "first Monday September"),
+        ("National Day for Truth and Reconciliation", "30th of September"),
+        ("Truth and Reconciliation Day", "30th of September"),
+        ("Thanksgiving", "second Monday of October"),
+        ("Halloween", "31st of October"),
+        ("Remembrance Day", "11th of November"),
+        ("Boxing Day", "26th of December")
+    ]
     
-    // Replace "new year" with "1st of January"
-    let finalQuery = modifiedQuery.replacingOccurrences(of: "new year", with: "1st of January", options: .caseInsensitive)
+    var modifiedQuery = query
     
-    // Replace "Islander Day" with "20th of February"
-    let updatedQuery = finalQuery.replacingOccurrences(of: "Islander Day", with: "20th of February", options: .caseInsensitive)
-    
-    // Replace "Family Day" with "20th of February"
-    let finalUpdatedQuery = updatedQuery.replacingOccurrences(of: "Family Day", with: "20th of February", options: .caseInsensitive)
-    
-    // Replace "Louis Riel Day" with "20th of February"
-    let finalQueryWithLouisRielDay = finalUpdatedQuery.replacingOccurrences(of: "Louis Riel Day", with: "20th of February", options: .caseInsensitive)
-    
-    // Replace "Heritage Day" with "20th of February"
-    let finalQueryWithHeritageDay = finalQueryWithLouisRielDay.replacingOccurrences(of: "Heritage Day", with: "20th of February", options: .caseInsensitive)
-    
-    // Replace "Valentine's Day" with "14th of February"
-    let finalQueryWithValentinesDay = finalQueryWithHeritageDay.replacingOccurrences(of: "Valentine's Day", with: "14th of February", options: .caseInsensitive)
-    
-    // Replace "St. Patrick's Day" with "17th of March"
-    let finalQueryWithStPatricksDay = finalQueryWithValentinesDay.replacingOccurrences(of: "St. Patrick's Day", with: "17th of March", options: .caseInsensitive)
-    
-    // Replace "Good Friday" with "7th of April"
-    let finalQueryWithGoodFriday = finalQueryWithStPatricksDay.replacingOccurrences(of: "Good Friday", with: "7th of April", options: .caseInsensitive)
-    
-    // Replace "Easter Monday" with "10th of April"
-    let finalQueryWithEasterMonday = finalQueryWithGoodFriday.replacingOccurrences(of: "Easter Monday", with: "10th of April", options: .caseInsensitive)
-    
-    // Replace "Mother's Day" with "14th of May"
-    let finalQueryWithMothersDay = finalQueryWithEasterMonday.replacingOccurrences(of: "Mother's Day", with: "14th of May", options: .caseInsensitive)
-    
-    // Replace "Victoria Day" with "22nd of May"
-    let finalQueryWithVictoriaDay = finalQueryWithMothersDay.replacingOccurrences(of: "Victoria Day", with: "22nd of May", options: .caseInsensitive)
-    
-    // Replace "Father's Day" with "18th of June"
-    let finalQueryWithFathersDay = finalQueryWithVictoriaDay.replacingOccurrences(of: "Father's Day", with: "18th of June", options: .caseInsensitive)
-    
-    // Replace "Aboriginal Day" with "21st of June"
-    let finalQueryWithAboriginalDay = finalQueryWithFathersDay.replacingOccurrences(of: "Aboriginal Day", with: "21st of June", options: .caseInsensitive)
-    
-    // Replace "St. Jean Baptiste Day" with "24th of June"
-    let finalQueryWithStJeanBaptisteDay = finalQueryWithAboriginalDay.replacingOccurrences(of: "St. Jean Baptiste Day", with: "24th of June", options: .caseInsensitive)
-    
-    // Replace "Canada Day" with "1st of July"
-    let finalQueryWithCanadaDay = finalQueryWithStJeanBaptisteDay.replacingOccurrences(of: "Canada Day", with: "1st of July", options: .caseInsensitive)
-    
-    // Replace "Civic Holiday" with "7th of August"
-    let finalQueryWithCivicHoliday = finalQueryWithCanadaDay.replacingOccurrences(of: "Civic Holiday", with: "7th of August", options: .caseInsensitive)
-    
-    // Replace "Labour Day" with "4th of September"
-    let finalQueryWithLabourDay = finalQueryWithCivicHoliday.replacingOccurrences(of: "Labour Day", with: "4th of September", options: .caseInsensitive)
-    
-    // Replace "National Day for Truth and Reconciliation" with "30th of September"
-    let finalQueryWithNationalDay = finalQueryWithLabourDay.replacingOccurrences(of: "National Day for Truth and Reconciliation", with: "30th of September", options: .caseInsensitive)
-    
-    // Replace "Truth and Reconciliation Day" with "30th of September"
-    let finalQueryWithTruthAndReconciliationDay = finalQueryWithNationalDay.replacingOccurrences(of: "Truth and Reconciliation Day", with: "30th of September", options: .caseInsensitive)
-    
-    // Replace "Thanksgiving" with "9th of October"
-    let finalQueryWithThanksgiving = finalQueryWithTruthAndReconciliationDay.replacingOccurrences(of: "Thanksgiving", with: "9th of October", options: .caseInsensitive)
-    
-    // Replace "Halloween" with "31st of October"
-    let finalQueryWithHalloween = finalQueryWithThanksgiving.replacingOccurrences(of: "Halloween", with: "31st of October", options: .caseInsensitive)
-    
-    // Replace "Remembrance Day" with "11th of November"
-    let finalQueryWithRemembranceDay = finalQueryWithHalloween.replacingOccurrences(of: "Remembrance Day", with: "11th of November", options: .caseInsensitive)
-    
-    // Replace "Boxing Day" with "26th of December"
-    let finalQueryWithBoxingDay = finalQueryWithRemembranceDay.replacingOccurrences(of: "Boxing Day", with: "26th of December", options: .caseInsensitive)
-    
-    print(chrono.parse(text: finalQueryWithBoxingDay))
-    
-    if finalQueryWithBoxingDay.lowercased().contains("25th of December") {
-        // Extract the year from the current date
-        let currentYear = Calendar.current.component(.year, from: Date())
-        
-        // Set the date components for Christmas
-        var christmasComponents = DateComponents(year: currentYear, month: 12, day: 25)
-        
-        // Check if the query contains "last 25th of December"
-        if finalQueryWithBoxingDay.lowercased().contains("last") {
-            // Subtract one year from the Christmas year
-            christmasComponents.year! -= 1
-        }
-        
-        // Find the date for Christmas
-        if let christmasDate = Calendar.current.date(from: christmasComponents) {
-            print(christmasDate)
-        }
-    } else if finalQueryWithBoxingDay.lowercased().contains("1st of January") {
-        // Extract the year from the current date
-        let currentYear = Calendar.current.component(.year, from: Date())
-        
-        // Set the date components for New Year
-        var newYearComponents = DateComponents(year: currentYear, month: 1, day: 1)
-        
-        // Check if the query contains "last 1st of January"
-        if finalQueryWithBoxingDay.lowercased().contains("last") {
-            // Subtract one year from the New Year year
-            newYearComponents.year! -= 1
-        }
-        
-        // Find the date for New Year
-        if let newYearDate = Calendar.current.date(from: newYearComponents) {
-            print(newYearDate)
-        }
-    } else if finalQueryWithBoxingDay.lowercased().contains("20th of February") {
-        // Extract the year from the current date
-        let currentYear = Calendar.current.component(.year, from: Date())
-        
-        // Set the date components for Islander Day
-        var islanderDayComponents = DateComponents(year: currentYear, month: 2, day: 20)
-        
-        // Check if the query contains "last 20th of February"
-        if finalQueryWithBoxingDay.lowercased().contains("last") {
-            // Subtract one year from the Islander Day year
-            islanderDayComponents.year! -= 1
-        }
-        
-        // Find the date for Islander Day
-        if let islanderDayDate = Calendar.current.date(from: islanderDayComponents) {
-            print(islanderDayDate)
-        }
-    } else {
-        print(chrono.parse(text: finalQueryWithBoxingDay))
+    for replacement in replacements {
+        modifiedQuery = modifiedQuery.replacingOccurrences(
+            of: replacement.pattern,
+            with: replacement.replacement,
+            options: .regularExpression,
+            range: nil
+        )
     }
+    
+    print(chrono.parse(text: modifiedQuery))
+    
+    if let year = extractYear(from: modifiedQuery) {
+        if let date = getDateForYear(year, modifiedQuery.lowercased().contains("last")) {
+            print(date)
+        }
+    }
+}
+
+func extractYear(from query: String) -> Int? {
+    let regex = try? NSRegularExpression(pattern: "\\d{4}")
+    let range = NSRange(location: 0, length: query.utf16.count)
+    let matches = regex?.matches(in: query, range: range)
+    
+    guard let firstMatch = matches?.first else {
+        return nil
+    }
+    
+    return Int((query as NSString).substring(with: firstMatch.range))
+}
+
+func getDateForYear(_ year: Int, _ isLastYear: Bool) -> Date? {
+    let currentDate = Date()
+    var dateComponents = Calendar.current.dateComponents([.day, .month], from: currentDate)
+    dateComponents.year = year - (isLastYear ? 1 : 0)
+    
+    return Calendar.current.date(from: dateComponents)
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -153,3 +89,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
